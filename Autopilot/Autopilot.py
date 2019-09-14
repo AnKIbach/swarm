a0a23f5d2ae2f89ca55e756142523b058812357b
Andreas Hellesnes <anhellesnes@oslo.mil.no>
2019-09-14 10:48:57 -0400

    call function autopilot fix #1

--- Autopilot/Autopilot.py
+++ Autopilot/Autopilot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import time
+import sys
 
+from Autopilot_callFunc import Autopilot
 from To_arduino import Arduino
-from PID import PID
 from GPS_definition import GPS
 from Vector_definition import Vector
 from ROS_operators.Navigation_data import navData
@@ -10,27 +11,40 @@ from ROS_operators.Navigation_data import navData
 
 def main():
     nav = navData()
-    controller = PID()
+    autopilot = Autopilot()
     arduino = Arduino()
     arduino.connect()
     arduino.neutral_start()
 
-    wantGPS = GPS(60.394238, 5.266250)
+    wanted_GPS = GPS(60.394238, 5.266250)
 
+    wait_time = 0.0
 
-    while True:
-        
-        curGPS = nav.get_GPS()
-        currentVect = nav.get_Vector()
-        wantedVec = wantGPS.calculate(curGPS)
-        wantedVec.showVector()
-        controller.set_wanted(wantedVec)
-        out = controller.update(currentVect)
-        out.showVector()
-        outfart = currentVect.magnitude + out.magnitude
-        arduino.update(outfart, out.angle)
+    while nav.is_ready() == False or arduino.is_ready() == False:
+    #waits for both systems to connect
+        wait_time += 0.1
         time.sleep(0.1)
+        if wait_time > 10.0: #exit if timeout is over 10s
+            sys.exit(0)
 
+    try:
+        while True:
+            current_GPS = nav.get_GPS()
+            current_vector = nav.get_Vector()
+
+            wanted_vector = wanted_GPS.calculate(current_GPS)
+
+            autopilot.set_wanted_vector(wanted_vector)
+            
+            change = autopilot(current_vector)
+
+            arduino.update(change.magnitude, change.angle)
+            time.sleep(0.1)
+
+    except KeyboardInterrupt:
+        print("exiting...")
+        sys.exit(0)
+        
 if __name__ == "__main__":
     main()
 
