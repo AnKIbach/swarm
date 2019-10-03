e7c678796af8276961d340cf01cceb6019d7c255
Andreas Hellesnes <anhellesnes@oslo.mil.no>
2019-10-03 11:38:19 -0400

    further testing

--- Autopilot/test.py
+++ Autopilot/test.py
@@ -7,15 +7,19 @@ import serial
 import pyfirmata
 
 def main():
+
     board = pyfirmata.Arduino('/dev/ttyACM1')
-    pin = board.get_pin('d:5:s')
 
-    try:
-        angle = int(input("enter angle: "))
-        pin.write(angle)
+    # iter8 = pyfirmata.util.Iterator(board)
+    # iter8.start()
+    pin = board.get_pin('d:9:s')
+    while True:
+        try:
+            angle = float(input("enter angle: "))
+            pin.write(angle)
 
-    except KeyboardInterrupt:
-        sys.exit()
+        except KeyboardInterrupt:
+            sys.exit()
 
 if __name__ == "__main__":
     main()
