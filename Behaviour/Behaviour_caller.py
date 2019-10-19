d0f33f1ba73154469374cf3aa52afd619eacf594
Andreas Hellesnes <anhellesnes@oslo.mil.no>
2019-10-19 14:20:46 -0400

    Ready for more work on functions

--- Behaviour/Behaviour_caller.py
+++ Behaviour/Behaviour_caller.py
@@ -10,7 +10,7 @@ from Behaviours import Boids
 from Behaviours import PSO
 
 class Behave: # funny :)
-    def __init__(self, use_behaviour = Boids ):
+    def __init__(self, use_behaviour = Boids):
         self._handle_behaviour(use_behaviour)
 
         self.guided_velocity = 0.0
@@ -36,14 +36,17 @@ class Behave: # funny :)
     def _handle_behaviour(self, chosen_behaviour):
         pass #masse if setninger?
 
-    def set_wanted_xy(self, velocity_east, velocity_north):
+    def _calculate_distances(self, global_list):
+        pass
+
+    def _use_xy(self, velocity_east, velocity_north):
         self.wanted_x = velocity_east
         self.wanted_y = velocity_north
         wanted_list = [self.wanted_x, self.wanted_y]
 
         self.controller.set_wanted(wanted_list)
 
-    def set_wanted_vector(self, wanted):
+    def _use_vector(self, wanted):
         self.guided_magnitude = wanted.magnitude
         self.guided_angle = wanted.angle
 
