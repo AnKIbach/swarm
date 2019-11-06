f0589adeba4dc71eadee3c13e148abf92658b772
Andreas Hellesnes <anhellesnes@oslo.mil.no>
2019-11-06 11:23:25 -0500

    fix

--- Behaviour/Behaviours/Boids.py
+++ Behaviour/Behaviours/Boids.py
@@ -77,7 +77,7 @@ class boidBehavior():
             separation = average_vector.__truediv__(total)#- self.movement
             separation_tot = m.sqrt(m.pow(separation.magnitude, 2)+m.pow(separation.angle, 2))
 
-            print("separation: {} : {} tot: {}", format(separation.magnitude, separation.angle, separation_tot))
+            print("separation: {} : {} tot: {}", separation.magnitude, separation.angle, separation_tot)
 
             if separation_tot > 0.0:
                 separation = (separation.__truediv__(separation_tot)) * self.maxForce
