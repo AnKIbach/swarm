0802b2c8eda8dc1d1e1daf4a6b53e0f03c6ad144
AnKIbach <50943920+AnKIbach@users.noreply.github.com>
2019-11-13 15:13:31 +0100

    Comments and docstrings added

--- Autopilot/Classes/Arduino_data.py
+++ Autopilot/Classes/Arduino_data.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+
+'''
+This is a class responsible for establishing a connection with an arduino through firmata
+
+It also converts a vector to the propper signals to actuate the motor and servo connected to ports
+5,6 9,10,11 on the arduino
+
+It contains these functions:
+    * 
+
+'''
 import time
 import sys
 
@@ -8,13 +19,20 @@ import pyfirmata
 from Ranger import autoRange
 
 class Arduino:
-    
+    '''Class resposible for compiling and sending data to arduino for motor and servo control'''
+
     def __init__(self, port, speedLimit = 1.0):
+        '''Creates a new connection with ardunio
+
+        Args:
+            port: string of serial port wich arduino should connected to, typically /dev/ttyACMx
+            speedLimit: integer that limits speed by procentage 0-100%
+        '''
+
         self.motor_wanted  = 0.0
         self.rudder_wanted = 0.0
         self.port = port
 
-        #rangers for converting speed to output
         self.range_motor  = autoRange(0.0,5.0,90,180) 
         self.range_rudder = autoRange(-45.0, 45.0, 60, 120)
 
@@ -46,6 +64,12 @@ class Arduino:
 
 
     def __call__(self, motor = 0.0, rudder = 0.0):
+        ''' call function to send new data to arduino
+        
+        Args:
+            motor: float between 0.0 and max speed of boat in m/s, set to 0.0 as standard
+            rudder: float between -45.0 and 45.0 in deg, set to 0.0 as standard
+        '''
         self.rudder_wanted  = round(self.range_rudder.new(rudder), 3)
         self.motor_wanted   = round(self.range_motor.new(motor * self.speed_limt), 3)
 
@@ -71,6 +95,11 @@ class Arduino:
             #catch error and return true value for started
 
     def get_error(self):
+        '''function to return error from arduino to user
+
+        Returns:
+            error message as string
+        '''
         return self.error
 
     def get_current(self, what = 0):
@@ -84,12 +113,27 @@ class Arduino:
             print("no value to return selected...")
 
     def connection_state(self):
+        '''Function to return connection state between RPi and Arduino
+
+        Returns:
+            connection state as boolean
+        '''
         return self.has_connection
 
     def start_state(self):
+        '''Function to return startup state from Arduino
+
+        Returns:
+            state as boolean
+        '''
         return self.started_correctly
 
     def is_ready(self):
+        '''Function to determine if arduino is ready to recieve data
+
+        Returns:
+            ready state as boolean
+        '''
         if self.has_connection == True and self.started_correctly == True:
             return True
         else:
