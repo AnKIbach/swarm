a195d07d2f6a786b56d8c26f3c622a0a00c3cbb1
AnKIbach <50943920+AnKIbach@users.noreply.github.com>
2019-11-17 17:18:36 +0100

    Fix for fence test on boat 1

--- Autopilot/ROS_operators/Autopilot_datasim.py
+++ Autopilot/ROS_operators/Autopilot_datasim.py
@@ -23,13 +23,13 @@ class Sim:
         self.curr = BoatOdometry()
 
         self.speed = 0.5
-        self.bearing = 340
+        self.bearing = 180
 
     def __call__(self):
         self.header = self._get_header()
 
         
-        # self._send1(0)
+        self._send1(0)
         self._send2(1)
         self._send3(2)
         self._send4(3)
