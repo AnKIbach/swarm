1a1c23c1f51b8eca7d6254853b7d67f29321ec60
KiAnBach <50943920+AnKIbach@users.noreply.github.com>
2019-11-20 13:36:31 +0100

    fix test

--- Behaviour/ROS_operators/Behaviour_sub.py
+++ Behaviour/ROS_operators/Behaviour_sub.py
@@ -27,8 +27,8 @@ class Subscriber:
         self.fence      = Position()
 
         self.static_fence = Position()
-        self.static_fence.latitude  = 60.364917
-        self.static_fence.longitude = 5.262909
+        self.static_fence.latitude  = 60.3681370
+        self.static_fence.longitude = 5.2536029
 
         self.new_command = False
 
