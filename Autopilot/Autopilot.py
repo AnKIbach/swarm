14689cf5adfe4c6e12c4084d810ecce399eee041
AnKIbach <50943920+AnKIbach@users.noreply.github.com>
2019-11-25 20:18:52 +0100

    Test program for data inn og ut

---
+++ Autopilot/Autopilot.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import time
+import sys
+import rospy
+
+from Classes.GPS_class import GPS
+from Classes.Vector_class import Vector
+from Classes.Arduino_data import Arduino
+from Autopilot_caller import Autopilot
+
+from ROS_operators.Autopilot_sub import swarmWanted
+from ROS_operators.Navigation_data import navData
+from ROS_operators.Autopilot_talker import Talker
+
+
+def main():
+    nav = navData()
+    autopilot = Autopilot()
+    time.sleep(0.2)
+
+    time.sleep(0.2)
+    #fix for test
+    autopilot_talker = Talker()
+    time.sleep(0.2)
+    wait_time, clicks = 0.0, 0
+    wanted_GPS = GPS(60.365625, 5.264544)
+
+    wanted_vector = Vector(0.4, 180.0)
+
+    while nav.is_ready() == False:
+    #waits for both systems to connect
+        wait_time += 0.1
+        time.sleep(0.1)
+        if wait_time > 10.0: #exit if timeout is over 10s
+            sys.exit(0)
+
+    if nav.is_ready() == True:
+        print("Pixhawk is connected and ready at: ", nav.mode)
+
+    print("entering autopilot loop...")
+
+    while not rospy.is_shutdown():
+        try:
+            # wanted_GPS = new_gps(wanted_GPS)
+
+            current_GPS = nav.get_GPS()
+            current_vector = nav.get_Vector()
+
+            autopilot.set_wanted_vector(wanted_vector)
+
+            change_vector = autopilot(current_vector)
+            
+            arduino(change_vector.magnitude, change_vector.angle) #possible addition of another dampening for angle
+            
+            #publishing to ROS
+            autopilot_talker(current_vector, 
+                            current_GPS,
+                            wanted_vector,
+                            wanted_GPS, #change for behaviour
+                            change_vector)
+
+            clicks += 1
+
+            time.sleep(0.2)
+
+        except rospy.ROSInterruptException():
+            arduino._start() 
+            sys.exit()
+        finally:
+            pass
+    arduino()
+
+if __name__ == "__main__":
+    main()
