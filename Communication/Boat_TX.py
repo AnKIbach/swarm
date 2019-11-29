9cd9ae1dd7fc4d52e8cacd15015797d45a0e940b
AnKIbach <50943920+AnKIbach@users.noreply.github.com>
2019-11-29 17:22:21 +0100

    Comments added

--- Communication/Boat_TX.py
+++ Communication/Boat_TX.py
@@ -26,6 +26,7 @@ def main():
     rospy.init_node('uav_tx_node', anonymous=True)
     rospy.logdebug("Started pos_udp node")
 
+    # defining variables for udp publisher
     mcast_grp  = rospy.get_param('~mcast_addr', "225.0.0.25")
     mcast_port = rospy.get_param('~mcast_port', 4243)
     compress   = rospy.get_param('~compression', False)
@@ -35,13 +36,16 @@ def main():
 
     ttl = rospy.get_param('~ttl', 1) #ttl - time to live for socket
 
+    #Definition of topic adresses for data to send
     odometry_topic    = rospy.get_param('~odometry_subscriber',      "/autopilot/current")
     status_topic      = rospy.get_param('~status_subscriber',    "/autopilot/status")
 
+    #Initialization of publisher socket
     publisher = PositionPublisher(mcast_grp, mcast_port,
             ttl=ttl, compress=compress, nav_hz = nav_hz, state_hz=state_hz,
             cpu_hz=cpu_hz)
 
+    #initialization of subscribers 
     rospy.Subscriber(odometry_topic,  BoatOdometry, publisher.handle_odometry)
     rospy.Subscriber(status_topic,    BoatStatus,   publisher.handle_boat_status)
 
